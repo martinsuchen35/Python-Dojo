7c456b9a8baed7db4aa2d4d7084397dbe0ac202c
Chen Su <martinsuchen3557@gmail.com>
2016-10-10 07:22:15 -0400

    Use numpy instead of alias.

--- UdacityIntro/naivebayes/naive_bayes_example.py
+++ UdacityIntro/naivebayes/naive_bayes_example.py
@@ -1,7 +1,7 @@
-import numpy as np
+import numpy
 
-features_train = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-labels_train = np.array([1, 1, 1, 2, 2, 2])
+features_train = numpy.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+labels_train = numpy.array([1, 1, 1, 2, 2, 2])
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 clf.fit(features_train, labels_train)
@@ -9,6 +9,6 @@ print(clf.predict([[-0.8, -1]]))
 print(clf.predict([[3, 4]]))
 
 clf_pf = GaussianNB()
-clf_pf.partial_fit(features_train, labels_train, np.unique(labels_train))
+clf_pf.partial_fit(features_train, labels_train, numpy.unique(labels_train))
 print(clf_pf.predict([[-0.8, -1]]))
 print(clf.predict([[3, 4]]))
