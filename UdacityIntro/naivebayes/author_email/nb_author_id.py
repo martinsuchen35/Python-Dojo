04d0514fc71dcbf4ecbbcc8978888768c070fd62
Chen Su <martinsuchen3557@gmail.com>
2016-10-12 10:31:28 -0400

    Refactor.

--- UdacityIntro/naivebayes/author_email/nb_author_id.py
+++ UdacityIntro/naivebayes/author_email/nb_author_id.py
@@ -28,26 +28,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+# USE NB
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
 t0 = time()
 clf.fit(features_train, labels_train)
-print "training time:", round(time() - t0, 3), "s"
+print "NB training time:", round(time() - t0, 3), "s"
 
 t1 = time()
 pred = clf.predict(features_test)
-print "prediction time:", round(time() - t1, 3), "s"
+print "NB prediction time:", round(time() - t1, 3), "s"
 
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(pred, labels_test)
 print(accuracy)
 
-#########################################################
 # no. of Chris training emails: 7936
 # no. of Sara training emails: 7884
 # training time: 1.635/1.742/1.607/1.509/1.684 s
 # prediction time: 0.145/0.165/0.13/0.135/0.148 s
 # 0.973265073948
+#########################################################
 
 
