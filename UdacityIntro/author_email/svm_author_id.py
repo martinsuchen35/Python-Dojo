e23eea6b96cecda94a2b937f09a33d79e34edc8b
Chen Su <martinsuchen3557@gmail.com>
2016-10-12 11:25:06 -0400

    Predict element.

--- UdacityIntro/author_email/svm_author_id.py
+++ UdacityIntro/author_email/svm_author_id.py
@@ -38,6 +38,10 @@ t = time()
 pred = clf.predict(features_test)
 print "SVC prediction time:", round(time() - t, 3), "s"
 
+print pred[10]
+print pred[26]
+print pred[50]
+
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(pred, labels_test)
 print(accuracy)
@@ -58,21 +62,21 @@ print(accuracy)
 #########################################################
 # One way to speed up an algorithm is to train it on a smaller training dataset.
 # The tradeoff is that the accuracy almost always goes down when you do this.
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 # These lines effectively slice the training dataset down to 1% of its original size, tossing out 99% of the training data.
 
-t = time()
-clf.fit(features_train, labels_train)
-print "SVC training time:", round(time() - t, 3), "s"
-
-t = time()
-pred = clf.predict(features_test)
-print "SVC prediction time:", round(time() - t, 3), "s"
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
+# t = time()
+# clf.fit(features_train, labels_train)
+# print "SVC training time:", round(time() - t, 3), "s"
+#
+# t = time()
+# pred = clf.predict(features_test)
+# print "SVC prediction time:", round(time() - t, 3), "s"
+#
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(pred, labels_test)
+# print(accuracy)
 
 # FOR LINEAR KERNEL
 # SVC training time: 0.092 s
@@ -81,86 +85,86 @@ print(accuracy)
 #########################################################
 
 #########################################################
-clf = SVC(kernel="rbf")
-
-t = time()
-clf.fit(features_train, labels_train)
-print "SVC training time:", round(time() - t, 3), "s"
-
-t = time()
-pred = clf.predict(features_test)
-print "SVC prediction time:", round(time() - t, 3), "s"
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
+# clf = SVC(kernel="rbf")
+#
+# t = time()
+# clf.fit(features_train, labels_train)
+# print "SVC training time:", round(time() - t, 3), "s"
+#
+# t = time()
+# pred = clf.predict(features_test)
+# print "SVC prediction time:", round(time() - t, 3), "s"
+#
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(pred, labels_test)
+# print(accuracy)
 
 # SVC training time: 0.101 s
 # SVC prediction time: 1.07 s
 # 0.616040955631
 #########################################################
-print "\nC=10.0"
-clf = SVC(C=10.0, kernel="rbf")
-
-t = time()
-clf.fit(features_train, labels_train)
-print "SVC training time:", round(time() - t, 3), "s"
-
-t = time()
-pred = clf.predict(features_test)
-print "SVC prediction time:", round(time() - t, 3), "s"
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
-
-
-print "\nC=100.0"
-clf = SVC(C=100.0, kernel="rbf")
-
-t = time()
-clf.fit(features_train, labels_train)
-print "SVC training time:", round(time() - t, 3), "s"
-
-t = time()
-pred = clf.predict(features_test)
-print "SVC prediction time:", round(time() - t, 3), "s"
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
-
-
-print "\nC=1000.0"
-clf = SVC(C=1000.0, kernel="rbf")
-
-t = time()
-clf.fit(features_train, labels_train)
-print "SVC training time:", round(time() - t, 3), "s"
-
-t = time()
-pred = clf.predict(features_test)
-print "SVC prediction time:", round(time() - t, 3), "s"
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
-
-
-print "\nC=10000.0"
-clf = SVC(C=10000.0, kernel="rbf")
-
-t = time()
-clf.fit(features_train, labels_train)
-print "SVC training time:", round(time() - t, 3), "s"
-
-t = time()
-pred = clf.predict(features_test)
-print "SVC prediction time:", round(time() - t, 3), "s"
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
+# print "\nC=10.0"
+# clf = SVC(C=10.0, kernel="rbf")
+#
+# t = time()
+# clf.fit(features_train, labels_train)
+# print "SVC training time:", round(time() - t, 3), "s"
+#
+# t = time()
+# pred = clf.predict(features_test)
+# print "SVC prediction time:", round(time() - t, 3), "s"
+#
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(pred, labels_test)
+# print(accuracy)
+#
+#
+# print "\nC=100.0"
+# clf = SVC(C=100.0, kernel="rbf")
+#
+# t = time()
+# clf.fit(features_train, labels_train)
+# print "SVC training time:", round(time() - t, 3), "s"
+#
+# t = time()
+# pred = clf.predict(features_test)
+# print "SVC prediction time:", round(time() - t, 3), "s"
+#
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(pred, labels_test)
+# print(accuracy)
+#
+#
+# print "\nC=1000.0"
+# clf = SVC(C=1000.0, kernel="rbf")
+#
+# t = time()
+# clf.fit(features_train, labels_train)
+# print "SVC training time:", round(time() - t, 3), "s"
+#
+# t = time()
+# pred = clf.predict(features_test)
+# print "SVC prediction time:", round(time() - t, 3), "s"
+#
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(pred, labels_test)
+# print(accuracy)
+#
+#
+# print "\nC=10000.0"
+# clf = SVC(C=10000.0, kernel="rbf")
+#
+# t = time()
+# clf.fit(features_train, labels_train)
+# print "SVC training time:", round(time() - t, 3), "s"
+#
+# t = time()
+# pred = clf.predict(features_test)
+# print "SVC prediction time:", round(time() - t, 3), "s"
+#
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(pred, labels_test)
+# print(accuracy)
 
 # C=10.0
 # SVC training time: 0.097 s
