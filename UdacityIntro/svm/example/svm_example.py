cb4f2595812b117d95bfbd11fc90f29d6ce59a68
Chen Su <martinsuchen3557@gmail.com>
2016-10-12 10:44:06 -0400

    Refactor.

--- UdacityIntro/svm/example/svm_example.py
+++ UdacityIntro/svm/example/svm_example.py
@@ -14,18 +14,3 @@ print(clf.support_)
 print "\nget number of support vectors for each class"
 print(clf.n_support_)
 
-#########################################################
-# USE SVM
-# from sklearn.svm import SVC
-# clf = SVC(kernel="linear")
-#
-# t2 = time()
-# clf.fit(features_train, labels_train)
-# print "SVC training time:", round(time() - t2, 3), "s"
-#
-# t3 = time()
-# pred = clf.predict(features_test)
-# print "SVC prediction time:", round(time() - t3, 3), "s"
-#
-# accuracy = accuracy_score(pred, labels_test)
-# print(accuracy)
