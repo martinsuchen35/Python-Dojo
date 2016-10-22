481ec5ec449ede48ca32ad32af11d2b429cb305f
Chen Su <martinsuchen3557@gmail.com>
2016-10-22 19:33:00 -0400

    Enron data navigation.

--- UdacityIntro/datasets_questions/explore_enron_data.py
+++ UdacityIntro/datasets_questions/explore_enron_data.py
@@ -19,7 +19,18 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# Q1: For each person, how many features are available?
 print enron_data
+# A1: check out enron_data.txt, 21 features.
 
+# Q2: The "poi" feature records whether the person is a person of interest, according to our definition.
+#       How many POIs are there in the E+F dataset?
+print enron_data["METTS MARK"]["poi"]
 
-
+count = 0
+for person_name in enron_data:
+    #print person
+    if enron_data[person_name]["poi"] == 1:
+        count += 1
+print count
+# A2: count = 18
